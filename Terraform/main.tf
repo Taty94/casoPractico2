8606bc71b7aca5da6c9e71d1fd14276dcafa95c0
Tatiana Montenegro <tmontene@pichincha.com>
2025-07-05 18:46:36 -0500

    Delete ¨double qoute

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 provider "azurerm" {
-    subscription_id = "var.tenant2_subscription_id"
-    tenant_id       = "var.tenant2_tenant_id"
+    subscription_id = var.subscription_id
+    tenant_id       = var.tenant_id
     features {}
 }
 
