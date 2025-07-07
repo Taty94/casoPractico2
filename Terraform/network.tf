9530b003db39697ab949625c1028f2353d781665
Tatiana Montenegro <tmontene@pichincha.com>
2025-07-07 11:51:21 -0500

    Add resources needed for VM

---
+++ Terraform/network.tf
@@ -0,0 +1,55 @@
+resource "azurerm_virtual_network" "vnet" {
+    name                = "vnet-casopractico2"
+    location            = var.location
+    resource_group_name = var.resource_group_name
+    address_space       = ["10.0.0.0/16"]
+
+    tags = {
+      environment = "casopractico2"
+    }
+}
+
+resource "azurerm_subnet" "subnet" {
+    name                 = "subnet-casopractico2"
+    resource_group_name  = var.resource_group_name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-casopractico2"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_network_security_rule" "inbound_rule" {
+  name                        = "InRule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "outbound_rule" {
+  name                        = "OutRule"
+  priority                    = 100
+  direction                   = "Outbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
