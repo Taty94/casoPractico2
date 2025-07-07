ba759b97a811aa94a0219954ead1f7bb36c1442a
Tatiana Montenegro <tmontene@pichincha.com>
2025-07-07 11:50:44 -0500

    usea vars in rg file

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "rg" {
-    name     = "rg-unir-tf"
-    location = "West Europe"
+    name     = var.resource_group_name
+    location = var.location
     tags = {
         environment = "casopractico2"
     }
