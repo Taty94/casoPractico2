23906cab57d55c8c91ae3367ff27745de8231ccf
Tatiana Montenegro <tmontene@pichincha.com>
2025-07-10 13:15:43 -0500

    Add dependson in vn and nsg

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -4,6 +4,8 @@ resource "azurerm_virtual_network" "vnet" {
     resource_group_name = var.resource_group_name
     address_space       = ["10.0.0.0/16"]
 
+    depends_on = [azurerm_resource_group.rg]
+
     tags = {
       environment = "casopractico2"
     }
@@ -20,6 +22,8 @@ resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-casopractico2"
   location            = var.location
   resource_group_name = var.resource_group_name
+
+  depends_on = [azurerm_resource_group.rg]
 
   tags = {
     environment = "casopractico2"
